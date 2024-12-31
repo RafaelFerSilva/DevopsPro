d7c95f6a133e0eb965565f2b7452e32313bcd235
RafaelFerSilva <todosomtodavoz@gmail.com>
2024-12-31 09:57:09 -0300

    List containers

--- Docker/DockerAPI_SDK/index.py
+++ Docker/DockerAPI_SDK/index.py
@@ -2,4 +2,9 @@ import docker
 
 client = docker.from_env()
 
-client.containers.run("nginx", detach=True, ports={"80/tcp":("0.0.0.0", 8080)})
+# client.containers.run("nginx", detach=True, ports={"80/tcp":("0.0.0.0", 8080)})
+
+containers_list = client.containers.list(all=True)
+
+for item in containers_list:
+    print(f'{item.id} - {item.image.tags} - {item.name}')
