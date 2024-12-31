85949e31932bdfbf79877ff21878cfc46f73c89b
RafaelFerSilva <todosomtodavoz@gmail.com>
2024-12-31 13:49:39 -0300

    gerenciar banco de dados com docker api

--- Docker/DockerAPI_SDK/index.py
+++ Docker/DockerAPI_SDK/index.py
@@ -2,16 +2,16 @@ import docker
 
 client = docker.from_env()
 
-# client.containers.run("nginx", detach=True, ports={"80/tcp":("0.0.0.0", 8080)})
+client.containers.run("nginx", detach=True, ports={"80/tcp":("0.0.0.0", 8080)})
 
-# containers_list = client.containers.list(all=True)
+containers_list = client.containers.list(all=True)
 
-# for item in containers_list:
-#     print(f'{item.id} - {item.image.tags} - {item.name}')
+for item in containers_list:
+    print(f'{item.id} - {item.image.tags} - {item.name}')
 
 container = client.containers.get("2a2bb0c8a933705f0c143fd35d3b2e44a15c326f3ddf7290d7b0f45b4feef968")
-# print(f'{container.id} - {container.image.tags} - {container.name}')
+print(f'{container.id} - {container.image.tags} - {container.name}')
 
-# print(container.logs())
+print(container.logs())
 
 container.remove(force=True)
