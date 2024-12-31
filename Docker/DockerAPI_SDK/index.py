5fd6e9b8a0cea46532da2ba36ee892b1f2a5da05
RafaelFerSilva <todosomtodavoz@gmail.com>
2024-12-31 09:59:00 -0300

    Get container by ID

--- Docker/DockerAPI_SDK/index.py
+++ Docker/DockerAPI_SDK/index.py
@@ -4,7 +4,10 @@ client = docker.from_env()
 
 # client.containers.run("nginx", detach=True, ports={"80/tcp":("0.0.0.0", 8080)})
 
-containers_list = client.containers.list(all=True)
+# containers_list = client.containers.list(all=True)
 
-for item in containers_list:
-    print(f'{item.id} - {item.image.tags} - {item.name}')
+# for item in containers_list:
+#     print(f'{item.id} - {item.image.tags} - {item.name}')
+
+container = client.containers.get("2a2bb0c8a933705f0c143fd35d3b2e44a15c326f3ddf7290d7b0f45b4feef968")
+print(f'{container.id} - {container.image.tags} - {container.name}')
